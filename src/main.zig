330fe38816ce30f2fb5cdbfd778aca769ae15bb6
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2024-12-01 22:50:54 -0500

    Responding with 200

--- src/main.zig
+++ src/main.zig
@@ -14,6 +14,7 @@ pub fn main() !void {
     });
     defer listener.deinit();
     //
-    _ = try listener.accept();
+    const connection = try listener.accept();
     try stdout.print("client connected!", .{});
+    try connection.stream.writer().writeAll("HTTP/1.1 200 OK\r\n\r\n");
 }
